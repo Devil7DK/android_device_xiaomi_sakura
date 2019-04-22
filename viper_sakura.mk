629684f15fe9a68ea6980f9c5eddba5c0075277b
Devil7DK <dineshthangavel47@gmail.com>
2019-04-22 09:14:05 +0000

    sakura: Update build fingerprint from latest dev build

--- viper_sakura.mk
+++ viper_sakura.mk
@@ -36,10 +36,10 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+    PRIVATE_BUILD_DESC="sakura_india-user 9 PKQ1.180917.001 9.4.18 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/sakura_india/sakura_india:9/PKQ1.180917.001/9.4.18:user/release-keys"
 
 # Its Official ViperOS
 VIPER_BUILD_TYPE := OFFICIAL
